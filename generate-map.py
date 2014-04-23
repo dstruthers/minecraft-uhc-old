9d1d67de08bc78d92f7b85d72b285cf2bf7c820d
Darren M. Struthers <dstruthers@gmail.com>
2014-04-23 11:07:04 -0700

    Initial commit

---
+++ generate-map.py
@@ -0,0 +1,62 @@
+import math, time
+from argparse import ArgumentParser
+from mc import MinecraftServer
+
+def log(msg):
+    """Log msg to stdout"""
+    now = time.strftime('%H:%M:%S')
+    print '[%s] [Terrain Generator] %s' % (now, msg)
+
+def wait_for_startup(server):
+    """Wait for a MinecraftServer to finish starting up"""
+    while True:
+        line = server.readline()
+        if line:
+            print line,
+            if line.find('Done') != -1:
+                log('Detected end of server initialization')
+                return
+
+argparser = ArgumentParser(description='Pre-generate Minecraft terrain over a \
+                                        given area')
+argparser.add_argument('--minecraft-dir', dest='minecraft_dir', required=True,
+                       help='Directory containing the Minecraft server .jar')
+argparser.add_argument('--jar', dest='minecraft_jar', required=True,
+                       help='Filename of Minecraft server .jar')
+argparser.add_argument('--map-size', dest='map_size', required=True, type=int,
+                       help='Desired width/height of terrain to generate')
+args = argparser.parse_args()
+
+adjusted_map_size = int(math.ceil(args.map_size / 192.0) * 192)
+
+server = MinecraftServer(args.minecraft_dir, args.minecraft_jar)
+spawn_coords_range = range(-(adjusted_map_size / 2) + 96,
+                           adjusted_map_size / 2 - 95,
+                           192)
+
+log('Will generate %(n)d x %(n)d area around map origin' % \
+    {'n': adjusted_map_size})
+log('Starting server')
+server.start()
+wait_for_startup(server)
+
+for x in spawn_coords_range:
+    for z in spawn_coords_range:
+        log('Changing world spawn point')
+        server.write('/setworldspawn %d 60 %d\n' % (x, z))
+        # Confirm spawn point was changed before stopping server
+        while server.is_running():
+            line = server.readline()
+            if line:
+                print line,
+                if line.find('Set the world spawn point') != -1:
+                    log('Stopping server')
+                    server.stop()
+        log('Server terminated with code %d' % server.returncode())
+
+        if x < max(spawn_coords_range) or z < max(spawn_coords_range):
+            log('Starting server')
+            server.start()
+            wait_for_startup(server)
+
+log('Finished generating terrain')
---
+++ mc.py
@@ -0,0 +1,40 @@
+import os, subprocess, time
+
+class MinecraftServer:
+    """Encapsulates a running Minecraft server"""
+
+    def __init__(self, minecraft_dir, minecraft_jar, java_opts = ''):
+        self.process = None
+        self.minecraft_dir = minecraft_dir
+        self.minecraft_jar = minecraft_jar
+        self.java_opts = java_opts
+
+    def start(self):
+        """Start the Minecraft server"""
+        os.chdir(self.minecraft_dir)
+        self.process = subprocess.Popen(['java', '-jar', self.minecraft_jar],
+                                        stdin=subprocess.PIPE,
+                                        stdout=subprocess.PIPE)
+
+    def stop(self):
+        """Stop the Minecraft server"""
+        if self.process:
+            self.process.stdin.write('/stop\n')
+                                        
+    def is_running(self):
+        """Returns True if server is running"""
+        self.process.poll()
+        return self.process.returncode == None
+
+    def returncode(self):
+        """Return code of stopped server, or None if server is still running"""
+        self.process.poll()
+        return self.process.returncode
+
+    def readline(self):
+        """Read a line of server output"""
+        return self.process.stdout.readline()
+
+    def write(self, input):
+        """Write to server's stdin"""
+        self.process.stdin.write(input)
